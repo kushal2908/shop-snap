da88a2336b72c4e480d8d1ca79136459725cbd8f
Safa <sardersafabinsalam@arogga.net>
2026-04-09 15:10:01 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,18 +91,45 @@ export default function RootLayout({
     return (
         <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
             <head>
-                <meta name="google-adsense-account" content="ca-pub-5391881178707156" />
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
                         __html: JSON.stringify(jsonLd),
                     }}
                 />
+                {/* Google Ads */}
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5391881178707156"
+                    crossOrigin="anonymous"
+                ></script>
+                {/* Google Tag Manager */}
+                <script>
+                    {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-T64T2TPP')`}
+                </script>
+
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-LJZ31EJGPK" />
+                <script>
+                    {` window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-LJZ31EJGPK')`}
+                </script>
             </head>
             <body className="min-h-full flex flex-col">
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-T64T2TPP"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
                 {children}
-                <GoogleAnalytics gaId={process.env.GA_ID as string} />
-                <GoogleTagManager gtmId={process.env.GA_ID as string} />
             </body>
         </html>
     );
